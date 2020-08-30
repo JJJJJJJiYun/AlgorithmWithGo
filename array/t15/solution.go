63719a44167a4dea9cbc8d0da72d88e9556e0647
jiyunhuang <jiyunhuang@tencent.com>
2020-08-30 19:57:32 +0800

    feat:nSum问题全解

--- array/t15/solution.go
+++ array/t15/solution.go
@@ -4,15 +4,21 @@ import (
 	"sort"
 )
 
+// 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+// 注意：答案中不可以包含重复的三元组。
+
+// 利用twoSum，但要注意去重
 func threeSum(nums []int) [][]int {
 	res := make([][]int, 0)
 	sort.Ints(nums)
 	for i := 0; i < len(nums); i++ {
 		num := nums[i]
+		// 只在比这个数大的数字中去找
 		twoSumRes := twoSum(nums[i+1:], -num)
 		for _, r := range twoSumRes {
 			res = append(res, append(r, num))
 		}
+		// 重复数字跳过
 		for i+1 < len(nums) && nums[i+1] == num {
 			i++
 		}
--- array/t18/solution.go
+++ array/t18/solution.go
@@ -2,6 +2,11 @@ package t18
 
 import "sort"
 
+// 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，
+// 使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+// 答案中不可以包含重复的四元组。
+
+// 思路和threeSum完全一样
 func fourSum(nums []int, target int) [][]int {
 	res := make([][]int, 0)
 	sort.Ints(nums)
