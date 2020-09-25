d0dbd6bbe8f075823e2a88419ea3fdd8d28f19fb
jiyunhuang <jiyunhuang@tencent.com>
2020-09-25 16:47:35 +0800

    feat:剑指offer t7 重建二叉树

--- tree/o7/solution.go
+++ tree/o7/solution.go
@@ -32,3 +32,31 @@ func ReconstructTreeWithPreAndInOrderTraversal(pre, in []int) *utils.TreeNode {
 	}
 	return helper(pre, in, 0, len(pre)-1, 0, len(in)-1)
 }
+
+// 重建二叉树，中序+后序
+
+// 9,3,15,20,7 in
+// 9,15,7,20,3 post
+func ReconstructTreeWithInAndPostOrderTraversal(in, post []int) *utils.TreeNode {
+	var helper func(in, post []int, is, ie, ps, pe int) *utils.TreeNode
+	helper = func(in, post []int, is, ie, ps, pe int) *utils.TreeNode {
+		if ps > pe {
+			return nil
+		}
+		node := &utils.TreeNode{Val: post[pe]}
+		nodeIndex := -1
+		for i := is; i <= ie; i++ {
+			if in[i] == node.Val {
+				nodeIndex = i
+				break
+			}
+		}
+		if nodeIndex == -1 {
+			panic(fmt.Errorf("invalid input"))
+		}
+		node.Left = helper(in, post, is, nodeIndex-1, ps, ps+nodeIndex-is-1)
+		node.Right = helper(in, post, nodeIndex+1, ie, ps+nodeIndex-is, pe-1)
+		return node
+	}
+	return helper(in, post, 0, len(in)-1, 0, len(post)-1)
+}
--- tree/o7/solution_test.go
+++ tree/o7/solution_test.go
@@ -30,3 +30,29 @@ func TestReconstructTreeWithPreAndInOrderTraversal(t *testing.T) {
 		})
 	}
 }
+
+func TestReconstructTreeWithInAndPostOrderTraversal(t *testing.T) {
+	type args struct {
+		in   []int
+		post []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *utils.TreeNode
+	}{
+		{
+			name: "case1",
+			args: args{
+				in:   []int{9, 3, 15, 20, 7},
+				post: []int{9, 15, 7, 20, 3},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ReconstructTreeWithInAndPostOrderTraversal(tt.args.in, tt.args.post)
+			utils.HierarchicalTraversalWithBreak(got)
+		})
+	}
+}
